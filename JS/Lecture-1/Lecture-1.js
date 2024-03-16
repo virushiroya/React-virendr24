3e24bb5883f8da526f229b6cabe498849063317c
virushiroya <virushiroya261@gmail.com>
2024-03-16 15:46:28 +0530

    16-3-2024

---
+++ JS/Lecture-1/Lecture-1.js
@@ -0,0 +1,77 @@
+document.getElementById("demo").innerHTML = "Hello, Good Morning!"
+
+/* Var, Let And Const */
+
+// Var - variables
+
+// Var is Global Variables Keyword
+
+// var Name = "Skillqode"
+// var Name = "City"
+
+// Variable variable-name = assignment value
+
+// console.log(Name);
+
+// {} scope
+
+// {
+//     var items ='toys';
+//     console.log(items);
+// }
+
+// var items1, items2, items3;
+
+// items1 = "items1"
+// items2 = "items2"
+// items = "items3"
+
+// console.log(items1);
+// console.log(items2);
+// console.log(items3);
+
+// let / const Keyword ECMA-6 (2015)
+// let And const is block-Scope Variables
+
+// let x = 10;
+// x = 20;
+// let y = 20;
+// let z = x + y;
+// console.log(z);
+
+// let x = 20;
+// let y = 20;
+
+// {
+//     console.log(x+y);
+// }
+
+{
+    let x = 20;
+    let y = 20;
+    console.log(x+y);
+}
+
+// let a, b, c, d;
+// a = 10;
+// b = "banana";
+// c = "cat";
+// d = "dog";
+
+// console.log(a+b+c+d);
+
+// Const Keyword
+
+// const x = "20";
+// const y = 40;
+
+// {
+//     console.log(x+y);
+// }
+
+// Javascript is Case-Sensitive Language
+
+// let x = 10;
+// let X = 20;
+
+// console.log(x);
